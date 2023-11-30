f0f27aad0c08ba60434c4401967587d70013b29d
chahla1 <cbouchot@myuax.com>
2023-11-30 13:53:36 +0100

    otro commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,24 @@
 #include <algorithm>
 #include <memory>
 #include <boost/lexical_cast.hpp>
+using namespace std;
+// Ejercicio 1: Vector e Iteradores
+void ejercicio1() {
+    cout << "Ejercicio 1: Vector e Iteradores" << endl;
+    std::vector<int> numeros = {10, 20, 30, 40, 50};
 
+    for(std::vector<int>::iterator it = numeros.begin(); it != numeros.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
+    cout << endl;
+}
+
+
+
+
+
+int main(){
+
+}
 
 
